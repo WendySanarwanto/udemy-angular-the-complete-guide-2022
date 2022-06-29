38fd2fb096b3bc7e6f4fef6d06ee370d28a23794
Wendy Sanarwanto <wendy.sanarwanto@gmail.com>
2022-06-29 16:05:33 +0700

    directives-deep-dive: Listenning to Directive's host element using HostListener decorator

--- directives-deep-dive/src/app/better-highlight/better-highlight.directive.ts
+++ directives-deep-dive/src/app/better-highlight/better-highlight.directive.ts
@@ -1,13 +1,22 @@
-import { Directive, ElementRef, OnInit, Renderer2 } from '@angular/core';
+import { Directive, ElementRef, HostListener, OnInit, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[appBetterHighlight]'
 })
 export class BetterHighlightDirective implements OnInit {
-
   constructor(private elementRef: ElementRef, private renderer: Renderer2) { }
 
   ngOnInit(): void {
-    this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'dodgerBlue');
+    // this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'dodgerBlue');
   }
+
+  @HostListener('mouseenter')
+  mouseOver(eventData: Event) {
+    this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'dodgerBlue');    
+  }
+
+  @HostListener('mouseleave')
+  mouseLeave(eventData: Event) {
+    this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'transparent');    
+  }  
 }
