bbb8cc15f303320b00489247c856cd097c7f7a12
Wendy Sanarwanto <wendy.sanarwanto@gmail.com>
2022-06-29 16:54:38 +0700

    directives-deep-dive: Added custom structural directive (appUnless)

--- directives-deep-dive/src/app/app.module.ts
+++ directives-deep-dive/src/app/app.module.ts
@@ -4,10 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BasicHightlightDirective } from './basic-highlight/basic-highlight.directive';
 import { AppComponent } from './app.component';
 import { BetterHighlightDirective } from './better-highlight/better-highlight.directive';
+import { UnlessDirective } from './unless/unless.directive';
 
 @NgModule({
   declarations: [
-    AppComponent, BasicHightlightDirective, BetterHighlightDirective
+    AppComponent, BasicHightlightDirective, BetterHighlightDirective, UnlessDirective
   ],
   imports: [
     BrowserModule
