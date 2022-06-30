ac462f04dd6ac606902690dd4d101febec12b979
Wendy Sanarwanto <wendy.sanarwanto@gmail.com>
2022-06-30 16:36:14 +0700

    my-recipes: Added Ingredients property on Recipe model and display it on recipe detail component

--- my-recipes/src/app/recipes/recipe.service.ts
+++ my-recipes/src/app/recipes/recipe.service.ts
@@ -1,4 +1,5 @@
 import { EventEmitter, Injectable } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 import { Recipe } from './recipe.model';
 
 @Injectable({
@@ -7,12 +8,21 @@ import { Recipe } from './recipe.model';
 export class RecipeService {
   onRecipeSelected: EventEmitter<Recipe> = new EventEmitter();
   private recipes: Recipe[] = [
-    new Recipe('Recipe Test #1',
-               'This is simply a test of Recipe #1.',
-               'https://awsimages.detik.net.id/community/media/visual/2019/09/13/bfb173c3-1e98-4cbf-9337-a7504399be26_11.jpeg'),
-    new Recipe('Recipe Test #2',
-               'This is simply a test of Recipe #2.',
-               'https://resepmembuat.com/wp-content/uploads/2015/01/resepmembuat.com-Nasi-Rawon.jpg')
+    new Recipe('Soto Ayam Lamongan',
+               'Soto Ayam khas Lamongan, Jawa Timur.',
+               'https://awsimages.detik.net.id/community/media/visual/2019/09/13/bfb173c3-1e98-4cbf-9337-a7504399be26_11.jpeg',
+               [
+                new Ingredient('Tumeric', 1),
+                new Ingredient('Egg', 1),
+                new Ingredient('Chickent Meat', 1)
+               ]),
+    new Recipe('Nasi Rawon',
+               'Nasi Rawon lezat khas Jawa Timur-an.',
+               'https://resepmembuat.com/wp-content/uploads/2015/01/resepmembuat.com-Nasi-Rawon.jpg',
+               [
+                new Ingredient('Beef Meat', 1),
+                new Ingredient('Egg', 1)
+               ])
   ];
 
   constructor() { }
