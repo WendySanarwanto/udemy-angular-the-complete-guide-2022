954fc6e1db48ca2e4796e97c2beec50b38302eff
Wendy Sanarwanto <wendy.sanarwanto@gmail.com>
2022-07-11 11:52:18 +0700

    forms-td-start: Implement form entry bindings, validation & submission handlings

--- forms-td-start/src/app/app.component.ts
+++ forms-td-start/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,7 +7,47 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  @ViewChild('myform')
+  private signupForm?: NgForm;
+  defaultQuestion: string = 'pet';
+  answer: string = '';
+  genders: string[] = ['male', 'female'];
+  user = {
+    username: '',
+    email: '',
+    secretQuestion: '',
+    answer: '',
+    gender: ''
+  };
+  submitted: boolean = false;
+
   suggestUserName() {
     const suggestedName = 'Superuser';
+    // this.signupForm?.setValue({
+    //   userData: {
+    //     username: suggestedName,
+    //     email: ''
+    //   },
+    //   secret: 'pet',
+    //   questionAnswer: '',
+    //   gender: 'male'
+    // });
+    this.signupForm?.form.patchValue({
+      userData: {
+        username: suggestedName,
+      }
+    });
+  }
+
+  onSubmit() {
+    // console.log(this.signupForm);
+    this.user.username = this.signupForm?.value.userData.username;
+    this.user.email = this.signupForm?.value.userData.email;
+    this.user.secretQuestion = this.signupForm?.value.secret;
+    this.user.answer = this.signupForm?.value.questionAnswer;
+    this.user.gender = this.signupForm?.value.gender;
+    this.submitted = true;
+    // TODO: Sending this.user to backend at here 
+    this.signupForm?.reset();
   }
 }
