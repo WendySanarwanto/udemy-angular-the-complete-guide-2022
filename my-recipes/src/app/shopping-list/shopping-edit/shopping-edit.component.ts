3483d3b11d308574edf687636b50bade9f2e5463
Wendy Sanarwanto <wendy.sanarwanto@gmail.com>
2022-07-12 10:50:51 +0700

    max-recipes: Applied Template Driven form approach on shopping-edit component

--- my-recipes/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ my-recipes/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,5 @@
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { Ingredient } from 'src/app/shared/ingredient.model';
 import { ShoppingListService } from '../shopping-list.service';
 
@@ -8,27 +9,37 @@ import { ShoppingListService } from '../shopping-list.service';
   styleUrls: ['./shopping-edit.component.css']
 })
 export class ShoppingEditComponent implements OnInit {
-  @ViewChild('nameInput', {static: false}) nameInputRef: ElementRef;
-  @ViewChild('amountInput', {static: false}) amountInputRef: ElementRef;
+  // @ViewChild('nameInput', {static: false}) nameInputRef: ElementRef;
+  // @ViewChild('amountInput', {static: false}) amountInputRef: ElementRef;
 
   constructor(private shoppingListService: ShoppingListService) { }
 
   ngOnInit(): void {
   }
 
-  onAddItem() {
+  onAddItem(form: NgForm) {
+    const value = form.value;
     this.shoppingListService.addIngredient(new Ingredient(
-      this.nameInputRef?.nativeElement.value,
-      this.amountInputRef?.nativeElement.value
+      value.name,
+      value.amount
+      // this.nameInputRef?.nativeElement.value,
+      // this.amountInputRef?.nativeElement.value
     ));
   }
 
-  onDeleteItem() {
-    this.shoppingListService.removeIngredient(this.nameInputRef?.nativeElement.value);
+  onDeleteItem(form: NgForm) {
+    // this.shoppingListService.removeIngredient(this.nameInputRef?.nativeElement.value);
+    this.shoppingListService.removeIngredient(form.value.name);
   }
 
-  onClearItem() {
-    this.nameInputRef.nativeElement.value = '';
-    this.amountInputRef.nativeElement.value = '';
+  onClearItem(form: NgForm) {
+    form.setValue({
+      'name': '',
+      'amount': ''
+    });
+
+    // this.nameInputRef.nativeElement.value = '';
+    // this.amountInputRef.nativeElement.value = '';
+
   }
 }
