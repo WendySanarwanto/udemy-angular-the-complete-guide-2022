d5b1366df29f48a153313a6a09b681b4c7d50a24
Wendy Sanarwanto <wendy.sanarwanto@gmail.com>
2022-07-14 16:33:43 +0700

    auth-01-auth-component: Implemented sign in

--- auth-01-auth-component/src/app/auth/auth.component.ts
+++ auth-01-auth-component/src/app/auth/auth.component.ts
@@ -29,22 +29,41 @@ export class AuthComponent {
     this.isLoading = true;
 
     if (!this.isLoginMode) {
-      try{
-        await this.authService.signUp(email, password);
-        form.reset();
-      } catch(err: any){
-        // Display nice error message to user
-        switch(err?.error?.error?.message) {
-          case 'EMAIL_EXISTS': 
-            this.error = 'This email exists already';
-            break;
-          default:
-            this.error = 'Last action got error.';
-        }
-        console.log(err);
-      } finally {
-        this.isLoading = false;
+      await this.doSignUp(email, password, form);
+    } else {
+      await this.doLogin(email, password, form);
+    }
+  }
+
+  private async doLogin(email: any, password: any, form: NgForm) {
+    try {
+      const loginResponse = await this.authService.login(email, password);
+      form.reset();
+      // return loginResponse;
+    } catch (err) {
+      // Display nice error message to user
+      this.error = 'Incorrect credential';
+    } finally {
+      this.isLoading = false;
+    }
+  }
+
+  private async doSignUp(email: any, password: any, form: NgForm) {
+    try {
+      await this.authService.signUp(email, password);
+      form.reset();
+    } catch (err: any) {
+      // Display nice error message to user
+      switch (err?.error?.error?.message) {
+        case 'EMAIL_EXISTS':
+          this.error = 'This email exists already';
+          break;
+        default:
+          this.error = 'Last action got error.';
       }
+      console.log(err);
+    } finally {
+      this.isLoading = false;
     }
   }
 }
--- auth-01-auth-component/src/app/auth/auth.service.ts
+++ auth-01-auth-component/src/app/auth/auth.service.ts
@@ -1,7 +1,8 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { lastValueFrom } from 'rxjs';
+import { lastValueFrom, Subject } from 'rxjs';
 import { environment } from '../../environments/environment';
+import { User } from './user.model';
 
 interface SignupResponse {
   idToken: string;
@@ -11,12 +12,18 @@ interface SignupResponse {
   localId: string
 } 
 
+export interface SignInResponse extends SignupResponse {
+  registered: boolean;
+}
+
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-  AUTH_BACKEND_URL: string = 'https://identitytoolkit.googleapis.com/v1/accounts:signUp';
+  SIGNUP_URL: string = 'https://identitytoolkit.googleapis.com/v1/accounts:signUp';
+  LOGIN_URL: string = 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword';
   API_KEY: string = environment.API_KEY;
+  user: Subject<User> = new Subject();
 
   constructor(private http: HttpClient) { }
 
@@ -24,9 +31,25 @@ export class AuthService {
     let params = new HttpParams();
     params  = params.append('key', this.API_KEY);
     const observableResponse = 
-      this.http.post<SignupResponse>(this.AUTH_BACKEND_URL, 
+      this.http.post<SignupResponse>(this.SIGNUP_URL, 
         { email, password, returnSecureToken: true }, { params});
     const response: SignupResponse = await lastValueFrom(observableResponse);
     console.log(response);
+  }
+
+  async login(email: string, password: string) {
+    let params = new HttpParams();
+    params  = params.append('key', this.API_KEY);
+    const observableResponse = 
+      this.http.post<SignInResponse>(this.LOGIN_URL, 
+        { email, password, returnSecureToken: true }, { params });
+    const response: SignInResponse = await lastValueFrom(observableResponse);
+    console.log(response);
+    this.user.next(new User(
+      response.email,
+      response.localId,
+      response.idToken,
+      new Date(new Date().getTime() + +response.expiresIn * 1000)
+    ));
   }
 }
